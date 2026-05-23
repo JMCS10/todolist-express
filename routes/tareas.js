c9f6831af472f4a6b8a24906cd4a1c6b0e92401a
Jherlan Marcelo Calero Sardan <jherlan.calero@gmail.com>
2026-05-23 19:55:05 -0400

    implementacion de http header y metadatos

--- routes/tareas.js
+++ routes/tareas.js
@@ -1,14 +1,25 @@
 var express = require("express");
 var router = express.Router();
 var Tarea = require("../models/tarea");
+
 //listar nuestras tareas
 router.get("/", function(req, res) {
   Tarea.find()
     .then(function(tareas) {
-      res.json(tareas);
+
+      res.set("Content-Type", "application/json");
+      res.set("X-Total-Count", tareas.length);
+
+      
+      res.json({
+        total: tareas.length,
+        fecha: new Date().toLocaleDateString(),
+        datos: tareas
+      });
+
     })
     .catch(function(err) {
-      res.json({ error: err.message });
+      res.status(500).json({ error: err.message });
     });
 });
 
@@ -26,12 +37,22 @@ router.post("/", function(req, res) {
 
   tarea.save()
     .then(function(tareaGuardada) {
-      res.json(tareaGuardada);
+
+     
+      res.set("Content-Type", "application/json");
+
+      res.status(201).json({
+        mensaje: "Tarea creada correctamente",
+        fecha: new Date().toLocaleDateString(),
+        datos: tareaGuardada
+      });
+
     })
     .catch(function(err) {
-      res.json({ error: err.message });
+      res.status(500).json({ error: err.message });
     });
 });
+
 
 router.put("/:id", function(req, res) {
   Tarea.findById(req.params.id)
@@ -40,10 +61,20 @@ router.put("/:id", function(req, res) {
       return tarea.save();
     })
     .then(function(tareaActualizada) {
-      res.json(tareaActualizada);
+
+      
+      res.set("Content-Type", "application/json");
+
+      
+      res.json({
+        mensaje: "Tarea actualizada correctamente",
+        fecha: new Date().toLocaleDateString(),
+        datos: tareaActualizada
+      });
+
     })
     .catch(function(err) {
-      res.json({ error: err.message });
+      res.status(500).json({ error: err.message });
     });
 });
 
@@ -51,10 +82,18 @@ router.put("/:id", function(req, res) {
 router.delete("/:id", function(req, res) {
   Tarea.findByIdAndDelete(req.params.id)
     .then(function() {
-      res.json({ mensaje: "Tarea eliminada correctamente" });
+
+      res.set("Content-Type", "application/json");
+
+      
+      res.json({
+        mensaje: "Tarea eliminada correctamente",
+        fecha: new Date().toLocaleDateString()
+      });
+
     })
     .catch(function(err) {
-      res.json({ error: err.message });
+      res.status(500).json({ error: err.message });
     });
 });
 
@@ -66,10 +105,19 @@ router.patch("/:id", function(req, res) {
       return tarea.save();
     })
     .then(function(tareaActualizada) {
-      res.json(tareaActualizada);
+
+    
+      res.set("Content-Type", "application/json");
+
+      res.json({
+        mensaje: "Estado actualizado correctamente",
+        fecha: new Date().toLocaleDateString(),
+        datos: tareaActualizada
+      });
+
     })
     .catch(function(err) {
-      res.json({ error: err.message });
+      res.status(500).json({ error: err.message });
     });
 });
 
